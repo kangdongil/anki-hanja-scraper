1b762e498afca1cbdeeeba840b150df6466dcf5f
Noru <akang8150@naver.com>
2024-01-24 10:28:01 +0900

    Parse Data Chunk through Regex Expression

--- src/components/input.py
+++ src/components/input.py
@@ -1,4 +1,34 @@
-def process_txt_file(file_path):
+import re
+
+
+def parse_data_by_regex(data, patterns):
+    result = {}
+    lines = data.strip().split("\n")
+
+    for i, pattern in enumerate(patterns):
+        if isinstance(pattern, str):
+            match = re.match(pattern, lines[i])
+            if match:
+                result.update(match.groupdict())
+        elif isinstance(pattern, tuple):
+            regex, delimiter, *rest = pattern
+            if rest:
+                raise ValueError(
+                    f"Invalid number of elements in tuple pattern at index {i}. Expected 2, got {len(pattern)}"
+                )
+            match = re.match(regex, lines[i])
+            if match:
+                result.update(match.groupdict())
+                key = list(match.groupdict().keys())[0]
+                if key in result:
+                    result[key] = result[key].split(delimiter)
+        else:
+            raise ValueError(f"Invalid datatype for pattern at index {i}")
+
+    return result
+
+
+def process_txt_file(file_path, patterns):
     with open(file_path, "r", encoding="utf-8") as file:
         content = file.read()
 
@@ -8,25 +38,19 @@ def process_txt_file(file_path):
 
     # Process each chunk and extract data into dictionaries
     for chunk in chunks:
-        lines = chunk.split("\n")
-        hanja, meaning, *simplified_chars = lines[0].split(":")
-        simplified_char = simplified_chars[0] if simplified_chars else None
-        words = lines[1].split(";")
-        reference_idx = lines[2]
-
-        entry = {
-            "hanja": hanja,
-            "meaning": meaning,
-            "simplified_char": simplified_char,
-            "words": words,
-            "reference_idx": reference_idx,
-        }
-
+        entry = parse_data_by_regex(chunk, patterns)
         processed_data.append(entry)
 
     return processed_data
 
 
 # Replace 'your_file.txt' with the path to your actual file
-result = process_txt_file("data/input/2401222258.txt")
-print(result)
+patterns = [
+    "(?P<hanja>[\w]):(?P<meaning>[\w\s.;]+):?(?P<simplified_char>[\w])?",
+    ("(?P<words>.*)", ";"),
+    "(?P<reference_idx>[\w]+)",
+]
+
+output = process_txt_file("data/input/2401222258.txt", patterns)
+
+print(output)
